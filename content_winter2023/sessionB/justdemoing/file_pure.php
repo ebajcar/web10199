5933dbb4720b77fd9665ca0beee2f0e0f1938025
Ellen Bajcar <ellen.bajcar@sheridaninstitute.ca>
2023-03-13 13:22:03 -0400

    Week 9 update

---
+++ content_winter2023/sessionB/justdemoing/file_pure.php
@@ -0,0 +1,14 @@
+<?php
+$x = 5;
+$y = 7.2;
+$z = $x + $y;
+echo '<!DOCTYPE html>
+    <html lang="en">
+        <body>
+            <p>';      
+            
+echo "The sum of " . $x . " and " . $y . " is " . $z;
+echo '          
+        </body>
+    </html>';
+?>
