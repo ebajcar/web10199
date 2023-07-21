59f5254e73d582bd7017b6db9e6589240605fe08
Ellen Bajcar <ellen.bajcar@sheridaninstitute.ca>
2023-07-21 08:54:48 -0400

    Create bajcar_syst10199.sql

---
+++ content_summer2023/worksheets/week11/bajcar_syst10199.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 20, 2023 at 04:41 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bajcar_syst10199`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `myguests`
+--
+
+CREATE TABLE `myguests` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `myguests`
+--
+
+INSERT INTO `myguests` (`id`, `firstname`, `lastname`) VALUES
+(1, 'Charlie', 'Duncan'),
+(2, 'Caroline', 'Smith');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `myguests`
+--
+ALTER TABLE `myguests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `myguests`
+--
+ALTER TABLE `myguests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
