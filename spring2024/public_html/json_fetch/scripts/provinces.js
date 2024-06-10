530cc407e96e3690dbb28fbb58904c8ef01713df
Ellen Bajcar <ellen.bajcar@sheridaninstitute.ca>
2024-06-10 17:53:48 -0400

    load json example

--- spring2024/public_html/json_fetch/scripts/provinces.js
+++ spring2024/public_html/json_fetch/scripts/provinces.js
@@ -1,9 +1,9 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
-    getMenu();
+    getProvinces();
 });
 
-function getMenu() {
+function getProvinces() {
     
     // fetch the .json file with the menu objects
     fetch("data/provinces.json")
@@ -13,22 +13,31 @@ function getMenu() {
         // when we get the json data, build the <select> and 
         // add it to the <header>
         .then(data => {
-
-            let selectList = document.createElement("select");
+            
+            let list = [];
+            //let selectList = document.createElement("select");
+            let curr = document.getElementById("prov")
 
             // for each item in the collection
             for (const item of data) {
 
+			curr.appendChild(document.createElement("option")); 
+			curr.setAttribute("value", item.postal);
+            curr.setAttribute("title", item.title);
+			curr.innerHTML = item.name + "  ("+ item.postal +")";
+			list[index] = item.name;
+            
+            
                 // make a link
-                let option = document.createElement("option");
+                //let option = document.createElement("option");
                 //a.href = link.file;
-                option.textContent = item.name;
+                //option.textContent = item.name;
 
                 // add link to nav
-                nav.append(a)
+                //nav.append(a)
             }
             // add nav to the header element
-            document.body.querySelector("prov").append(selectList);
+            document.body.querySelector("prov").append(list);
         // for debugging
         }).catch(error => {
             console.log(error);
