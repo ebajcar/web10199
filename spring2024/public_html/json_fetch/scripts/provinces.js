e3dd692e006c2f37e6ac8efc49457bd6ae4739b3
Ellen Bajcar <ellen.bajcar@sheridaninstitute.ca>
2024-06-10 19:16:38 -0400

    Update provinces.js

--- spring2024/public_html/json_fetch/scripts/provinces.js
+++ spring2024/public_html/json_fetch/scripts/provinces.js
@@ -4,23 +4,29 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function getProvinces() {
-    // fetch the .json file with the option objects
+    console.log("entered getProvinces");
+    // fetch the .json file with the menu objects
     fetch("data/provinces.json")
         // when the response arrives, parse it as json data
         .then(response => response.json())
         
-        // when we get the json data, build the <option> and 
-        // add it to the <select id="prov">
+        // when we get the json data, build the <select> and 
+        // add it to the <header>
         .then(data => {
+            console.log("attempting to get data");
+            //let list = [];
+            //let selectList = document.createElement("select");
             let parent = document.getElementById("prov");
+            console.log(parent);
+            let index = 0;
             // for each item in the collection
             for (const item of data) {
                 let curr = parent.appendChild(document.createElement("option")); 
                 curr.setAttribute("value", item.postal);
                 curr.setAttribute("title", item.title);
-                curr.innerText = item.name + "  ("+ item.postal +")";
+                curr.innerHTML = item.name + "  ("+ item.postal +")";
+                index++;
             }
-            console.log(parent);
         // for debugging
         }).catch(error => {
             console.log(error);
